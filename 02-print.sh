757a6f4160ce55e1b9d6da4f693c304b5251009c
VJ <vijen.negi@gmail.com>
2019-11-28 15:14:31 -0500

    added colors

--- 02-print.sh
+++ 02-print.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 echo " Using the echo command"
+echo -e "\e[31mThis is the warning sign in RED\e[0m"
+echo -e "\e[32mThis is the garden color in Green\e[0m"
+echo -e "\e[44mThis is the sky color for backgroud\e[0m"
